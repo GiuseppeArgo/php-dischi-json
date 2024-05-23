b867fb80ce5d4d85dc7dfa945aa4bca0a7e2b65d
GiuseppeArgo <giuseppe.argo18@gmail.com>
2024-05-23 19:26:58 +0200

    feat: complete project

--- js/script.js
+++ js/script.js
@@ -3,8 +3,15 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            discs: [],
         }
     },
-    created(){
+    created() {
+        axios
+        .get("http://localhost/boolean/php-dischi-json/server.php")
+        .then((resp) => {
+            console.log(resp);
+            this.discs = resp.data
+        })
     }
 }).mount("#app");
